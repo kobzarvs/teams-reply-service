c567ac8f99836504cbec86d01b0556a1de487969
Valeriy Kobzar <kobzarvs@gmail.com>
2021-07-02 10:22:14 +0000

    wip

--- api/reply.js
+++ api/reply.js
@@ -1,6 +1,6 @@
 module.exports = (req, res) => {
     res.json({
-      body: req.body,
+      body: req.body ? JSON.parse(req.body) : undefined,
       query: req.query,
       cookies: req.cookies,
     })
