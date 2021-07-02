b2359ae01d1fa1f08760a37f31d2dfbcabebf964
Valeriy Kobzar <kobzarvs@gmail.com>
2021-07-02 11:38:06 +0000

    ms botbuilder

--- api/reply.js
+++ api/reply.js
@@ -1,9 +1,19 @@
+const { TeamsActivityHandler, CardFactory } = require("botbuilder");
+
+
 module.exports = (req, res) => {
+  const response = {
+    composeExtension: {
+      type: "result",
+      attachmentLayout: "list",
+      attachments: [CardFactory.heroCard(obj.name, obj.description)],
+    },
+  }
   console.log('body', req.body);
     res.json({
-      body: req.body,
+      body: JSON.stringify(response),
       query: req.query,
       cookies: req.cookies,
-    })
+    });
   }
   
